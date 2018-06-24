b94ec6a4bc48f034a757322bbb54d292f47d218f
Joh Johannsen <jojolosangeles2017@gmail.com>
2018-06-24 07:47:33 -0700

    first commit

---
+++ __main__.py
@@ -0,0 +1,268 @@
+"""Executable suffix tree"""
+import sys
+from suffix_tree.suffix_tree import TreeBuilder, NodeStr
+
+builder = TreeBuilder((c for c in "mississippi"))
+
+"""
+Build a tree from sequence 'mississippi'
+                            01234567890
+
+Nodes other than root have an incoming edge represented by an inclusive range
+going from incoming_edge_start_offset to incoming_edge_end_offset.
+
+Root has values (-1,-1) for this range.
+
+Leaf has values (incoming_edge_start_offset, -1) for this range.
+
+"""
+
+class Verifier:
+    def __init__(self, root, location):
+        self.root = root
+        self.location = location
+        self.nodeState = {}
+        self.set_state(None, (-1, -1))
+
+    def set_state(self, k, v):
+        self.nodeState[k] = v
+
+    def replace_state(self, old_traversal, new_traversal):
+        self.nodeState[new_traversal] = self.nodeState[old_traversal]
+        self.nodeState.pop(old_traversal, None)
+
+    def traverse_to(self, traversal):
+        node = self.root
+        if traversal != None:
+            for key in list(traversal):
+                node = node.children[key]
+        return node
+
+    def verify(self):
+        for traversal in self.nodeState:
+            self.verifyNode(traversal, self.traverse_to(traversal), *self.nodeState[traversal])
+
+    def expect_equal(self, loc, traversal, expected, actual):
+        if expected != actual:
+            print("** FAIL {}: {} expected {}, got {}".format(loc, traversal, expected, actual))
+
+    def verifyNode(self, traversal, node, start_offset, end_offset):
+        self.expect_equal("node {} start after '{}'".format(node, traversal), traversal, start_offset, node.incoming_edge_start_offset)
+        self.expect_equal("node {} end after '{}'".format(node, traversal), traversal, end_offset, node.incoming_edge_end_offset)
+
+    def verifyLocation(self, traversal, on_node, value_offset):
+        node = self.traverse_to(traversal)
+        self.expect_equal("loc node after '{}'".format(traversal), traversal, node, self.location.node)
+        self.expect_equal("loc on_node after '{}'".format(traversal), traversal, on_node, self.location.on_node)
+        self.expect_equal("loc value_offset after '{}'".format(traversal), traversal, value_offset, self.location.data_source_value_offset)
+
+def edge_with_loc(start_offset, value_offset, str):
+    str_offset = value_offset - start_offset
+    return "{}({}){}".format(str[:str_offset], str[str_offset:str_offset+1], str[str_offset+1:])
+    #return "edge_with_loc {}, str_offset={}".format(str, str_offset)
+
+nodestr = NodeStr(builder.data_store)
+def size_of(node):
+    return sys.getsizeof(node) + sum(list(map(lambda x: size_of(x), node.children.values())), 0)
+
+def printTree(root):
+    print(nodestr.tree_str(root))
+    print("Location: {}, {}".format(nodestr.node_str(location.node), edge_with_loc(location.node.incoming_edge_start_offset, location.data_source_value_offset, nodestr.edge_str(location.node))))
+    print("tree size={}".format(size_of(root)))
+
+print("START TEST")
+print("..verify root initial state")
+root = builder.root
+location = builder.location
+verifier = Verifier(root, location)
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+print("..add 'm'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.set_state("m", (0, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+print("..add 'i'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.set_state("i", (1, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+print("..add 's'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.set_state("s", (2, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+print("..add 's'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("s", False, 2)
+
+print("..add 'i'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("s", (2, 2))
+verifier.set_state("ss", (3, -1))
+verifier.verify()
+verifier.verifyLocation("s", True, 2)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+
+verifier.set_state("si", (4, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("i", False, 1)
+
+print("..add 's'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("i", False, 2)
+
+print("..add 's'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("i", False, 3)
+
+print("..add 'i'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("i", False, 4)
+
+print("..add 'p'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("i", (1, 4))
+verifier.set_state("is", (5, -1))
+verifier.verify()
+verifier.verifyLocation("i", True, 4)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("ip", (8, -1))
+verifier.verify()
+verifier.verifyLocation("ss", False, 4)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("ss", (3, 4))
+verifier.set_state("sss", (5, -1))
+verifier.verify()
+verifier.verifyLocation("ss", True, 4)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("ssp", (8, -1))
+verifier.verify()
+verifier.verifyLocation("si", False, 4)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("si", (4, 4))
+verifier.set_state("sis", (5, -1))
+verifier.verify()
+verifier.verifyLocation("si", True, 4)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("sip", (8, -1))
+verifier.verify()
+verifier.verifyLocation("i", False, 1)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.replace_state("is", "iss")
+verifier.replace_state("ip", "isp")
+verifier.set_state("i", (1, 1))
+verifier.set_state("is", (2, 4))
+verifier.verify()
+verifier.verifyLocation("i", True, 1)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("ip", (8, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.set_state("p", (8, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+print("..add 'p'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == False)
+
+verifier.verify()
+verifier.verifyLocation("p", False, 8)
+
+print("..add 'i'")
+value,offset = builder.next()
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("p", (8, 8))
+verifier.set_state("pp", (9, -1))
+verifier.verify()
+verifier.verifyLocation("p", True, 8)
+
+result = builder.step(value,offset)
+assert(result == True)
+
+verifier.set_state("pi", (10, -1))
+verifier.verify()
+verifier.verifyLocation(None, True, -1)
+
+result = builder.step(value,offset)
+assert(result == False)
+verifier.verify()
+verifier.verifyLocation("i", True, 1)
+
+builder.finish()
+printTree(root)
+
+print("END TEST")
---
+++ suffix_tree/suffix_tree.py
@@ -0,0 +1,258 @@
+class DataStore:
+    """Stores values as they are encountered, provides access to previous values"""
+    def __init__(self):
+        self.values = []
+
+    def add(self, value):
+        self.values.append(value)
+
+    def value_str(self, start_offset, end_offset):
+        if end_offset == -1:
+            return ''.join(self.values[start_offset:])
+        else:
+            return ''.join(self.values[start_offset:end_offset + 1])
+
+    def value_at(self, offset):
+        return self.values[offset]
+
+
+class Node:
+    UNDEFINED_OFFSET = -1
+    id = 0
+
+    """Node represents an offset in a sequence of values."""
+    def __init__(self, incoming_edge_start_offset, incoming_edge_end_offset, children, suffix_link=None):
+        self.incoming_edge_start_offset = incoming_edge_start_offset
+        self.incoming_edge_end_offset = incoming_edge_end_offset
+        self.children = children
+        self.suffix_link = suffix_link
+        self.node_id = Node.id
+        Node.id += 1
+
+    def is_root(self):
+        return self.incoming_edge_start_offset == Node.UNDEFINED_OFFSET and \
+               self.incoming_edge_end_offset == Node.UNDEFINED_OFFSET and \
+               self.suffix_link == self
+
+    def is_internal(self):
+        return self.incoming_edge_start_offset >= 0 and \
+               self.incoming_edge_end_offset >= 0 and \
+               len(self.children) > 0
+
+    def is_leaf(self):
+        return len(self.children) == 0 and \
+               self.incoming_edge_start_offset >= 0 and \
+               self.incoming_edge_end_offset == Node.UNDEFINED_OFFSET
+
+    @property
+    def incoming_edge_length(self):
+        return self.incoming_edge_end_offset - self.incoming_edge_start_offset + 1
+
+
+class RootNode(Node):
+    def __init__(self):
+        super().__init__(-1, -1, {}, self)
+
+
+class LeafNode(Node):
+    def __init__(self, incoming_edge_start_offset, suffix_offset):
+        super().__init__(incoming_edge_start_offset, -1, {})
+        self.suffix_offset = suffix_offset
+
+
+class NodeStr:
+    """Create string forms of a node or tree.
+
+    Not using __repr__ or __str__ because this nodes only store value offsets,
+    actual values are in DataStore instance"""
+    def __init__(self, data_store):
+        self.data_store = data_store
+
+    def tree_str(self, node):
+        return_strings = [node.__repr__()]
+        return self.indent(return_strings, node.children, "  ")
+
+    def suffix_link_str(self, node):
+        if node.suffix_link == None:
+            return "=> needs suffix link"
+        else:
+            return "=> n{}".format(node.suffix_link.node_id)
+
+    def node_str(self, node):
+        if node.is_leaf():
+            return "({}) Leaf.{} {}, suffix_offset {}".format(
+                node.node_id,
+                node.incoming_edge_start_offset,
+                self.data_store.value_str(node.incoming_edge_start_offset, node.incoming_edge_end_offset),
+                node.suffix_offset)
+        elif node.is_root():
+            return "({}) Root".format(node.node_id)
+        else:
+            return "({}) Internal.{}.{} {} {}".format(node.node_id, node.incoming_edge_start_offset,
+                                                      node.incoming_edge_end_offset,
+                                                      self.edge_str(node),
+                                                      self.suffix_link_str(node))
+
+    def edge_str(self, node):
+        return self.data_store.value_str(node.incoming_edge_start_offset, node.incoming_edge_end_offset)
+
+    def indent(self, strings, children, prefix):
+        if children != None:
+            for child in children.values():
+                strings.append(prefix + self.node_str(child))
+                self.indent(strings, child.children, "  " + prefix)
+        return "\n".join(strings)
+
+
+class Location:
+    """A location is a specific value offset within a sequence.
+
+    This could be a Node's value_offset, or it could be an offset
+    in a Node's incoming edge."""
+    def __init__(self, node):
+        self.node = node
+        self.data_source_value_offset = -1
+        self.node_needing_suffix_link = None
+
+    @property
+    def data_source_value_offset(self):
+        return self._data_source_value_offset
+
+    def update_node_needing_suffix_link(self, new_node):
+        if self.node_needing_suffix_link != None:
+            self.node_needing_suffix_link.suffix_link = new_node
+        self.node_needing_suffix_link = new_node
+
+    def set_suffix_link(self):
+        if self.node_needing_suffix_link != None and self.on_node:
+            self.node_needing_suffix_link.suffix_link = self.node
+            self.node_needing_suffix_link = None
+
+    @data_source_value_offset.setter
+    def data_source_value_offset(self, data_source_value_offset):
+        self.on_node = (self.node.incoming_edge_end_offset == data_source_value_offset)
+        self._data_source_value_offset = data_source_value_offset
+
+    def __repr__(self):
+        if self.on_node:
+            return "location: {!r}".format(self.node)
+        else:
+            return "location: {!r}[{}]".format(self.node, self.data_source_value_offset)
+
+
+class NodeBuilder:
+    def __init__(self, location):
+        self.location = location
+        self.suffix_offset = 0
+
+    def add_leaf(self, node, value, offset):
+        leaf = LeafNode(offset, self.suffix_offset)
+        self.suffix_offset += 1
+        self.add_child(node, value, leaf)
+
+    def add_child(self, node, value, child):
+        node.children[value] = child
+        child.parent = node
+
+    def add_internal(self, value, node, value2, end_offset):
+        new_node = Node(node.incoming_edge_start_offset, end_offset, {}, None)
+        node.incoming_edge_start_offset = end_offset + 1
+        self.add_child(node.parent, value, new_node)
+        self.add_child(new_node, value2, node)
+        self.location.update_node_needing_suffix_link(new_node)
+        return new_node
+
+
+class TreeBuilder:
+    def __init__(self, data_generator, terminal_value=-1):
+        self.root = RootNode()
+        self.location = Location(self.root)
+        self.data_generator = data_generator
+        self.next_offset = 0
+        self.node_builder = NodeBuilder(self.location)
+        self.terminal_value = terminal_value
+        self.data_store = DataStore()
+
+    def next(self):
+        value = next(self.data_generator)
+        offset = self.next_offset
+        self.data_store.add(value)
+        self.next_offset += 1
+        return value,offset
+
+    def finish(self):
+        value = self.terminal_value
+        offset = self.next_offset
+        self.run_step(value, offset)
+
+    def run_step(self, value, offset):
+        while self.step(value, offset):
+            pass
+
+    def step(self, value, offset):
+        if self.location.on_node:
+            if value in self.location.node.children:
+                self.location.node = self.location.node.children[value]
+                self.location.data_source_value_offset = self.location.node.incoming_edge_start_offset
+                return False
+            else:
+                self.node_builder.add_leaf(self.location.node, value, offset)
+                result = self.traverse_to_next_suffix()
+                return result
+        elif value == self.data_store.value_at(self.location.data_source_value_offset + 1):
+            self.location.data_source_value_offset += 1
+            return False
+        else:
+            self.location.node = self.node_builder.add_internal(
+                self.data_store.value_at(self.location.node.incoming_edge_start_offset),
+                self.location.node,
+                self.data_store.value_at(self.location.data_source_value_offset + 1),
+                self.location.data_source_value_offset)
+            self.location.on_node = True
+            return True
+        raise ValueError
+
+    def traverse_to_next_suffix(self):
+        """
+        Traverse to the suffix for this node.
+
+        Return:
+                True if the location changed, otherwise False
+        """
+        next_node = self.location.node.suffix_link
+        if next_node == self.location.node:
+            return False
+        elif next_node == None:
+            # we have no suffix link, traverse up
+            amount_to_traverse = self.location.node.incoming_edge_length
+            parent = self.location.node.parent
+            value_offset = self.location.node.incoming_edge_start_offset
+            # the root suffix link is to itself, so we have to manually skip first character
+            if parent.suffix_link == parent:
+                value_offset += 1
+                amount_to_traverse -= 1
+            start_node = parent.suffix_link
+            self.traverse_down(start_node,
+                               value_offset,
+                               amount_to_traverse)
+        else:
+            self.location.node = next_node
+
+        self.location.set_suffix_link()
+        return True
+
+    def traverse_down(self, node, offset, amount_to_traverse):
+        if amount_to_traverse == 0:
+            self.location.node = node
+            self.location.data_source_value_offset = node.incoming_edge_end_offset
+        else:
+            self.location.node = node.children[self.data_store.value_at(offset)]
+            if self.location.node.is_leaf() or self.location.node.incoming_edge_length >= amount_to_traverse:
+                self.location.data_source_value_offset = self.location.node.incoming_edge_start_offset + amount_to_traverse - 1
+            else:
+                edge_length = self.location.node.incoming_edge_length
+                amount_to_traverse -= edge_length
+                return self.traverse_down(self.location.node, offset + edge_length, amount_to_traverse)
+
+    def __repr__(self):
+        return "{!r}".format(self.location)
